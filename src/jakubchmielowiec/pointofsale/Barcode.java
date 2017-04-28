f3c2b8a39f5a0bc55eeedf7c730f945f17bf4bcb
WinAnyway <kuba.chmielowiec@gmail.com>
2017-04-28 14:01:47 +0200

    ConsoleBarcodeScanner implemented

--- src/jakubchmielowiec/pointofsale/Barcode.java
+++ src/jakubchmielowiec/pointofsale/Barcode.java
@@ -1,4 +1,14 @@
 package jakubchmielowiec.pointofsale;
 
 public class Barcode {
+
+    private String code;
+
+    public Barcode(String input) {
+        this.code = input;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
--- src/jakubchmielowiec/pointofsale/ConsoleBarcodeScanner.java
+++ src/jakubchmielowiec/pointofsale/ConsoleBarcodeScanner.java
@@ -1,10 +1,19 @@
 package jakubchmielowiec.pointofsale;
 
+import java.util.Scanner;
+
 public class ConsoleBarcodeScanner implements BarcodeScanner {
+
+    Scanner scanner;
+
+    public ConsoleBarcodeScanner() {
+        scanner = new Scanner(System.in);
+    }
 
     @Override
     public Barcode scan() {
-        return null;
+        String input = scanner.next();
+        return new Barcode(input);
     }
 
 }
