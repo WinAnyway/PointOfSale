8f30ed9153d4ee6a3c04ce743fdbd5e1ffda1f72
WinAnyway <kuba.chmielowiec@gmail.com>
2017-04-28 17:05:48 +0200

    ConsoleReceiptPrinter, Receipt implemented

--- src/jakubchmielowiec/pointofsale/ConsoleReceiptPrinter.java
+++ src/jakubchmielowiec/pointofsale/ConsoleReceiptPrinter.java
@@ -1,10 +1,17 @@
 package jakubchmielowiec.pointofsale;
 
+import java.text.NumberFormat;
+
 public class ConsoleReceiptPrinter implements ReceiptPrinter {
 
     @Override
     public void printReceipt(Receipt receipt) {
-
+        System.out.println("Receipt");
+        System.out.println("Products:");
+        for (Product product: receipt.getProducts())
+            System.out.println(String.format("Name: %s Price: %s",
+                    product.getName(), NumberFormat.getCurrencyInstance().format(product.getPrice())));
+        System.out.println(String.format("Total Price: %s", NumberFormat.getCurrencyInstance().format(receipt.calculateTotalPrice())));
     }
 
 }
--- src/jakubchmielowiec/pointofsale/Receipt.java
+++ src/jakubchmielowiec/pointofsale/Receipt.java
@@ -1,13 +1,29 @@
 package jakubchmielowiec.pointofsale;
 
 import java.math.BigDecimal;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Receipt {
+
+    private List<Product> products;
+
+    public Receipt() {
+        products = new LinkedList<>();
+    }
+
     public BigDecimal calculateTotalPrice() {
-        return null;
+        BigDecimal total = BigDecimal.valueOf(0);
+        for (Product product : products)
+            total = total.add(product.getPrice());
+        return total;
     }
 
     public void addProduct(Product product) {
+        products.add(product);
+    }
 
+    public List<Product> getProducts() {
+        return products;
     }
 }
